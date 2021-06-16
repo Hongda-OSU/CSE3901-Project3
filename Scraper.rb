5d720a2eeef7d60d25babc1f88d1e1f589c6b8ae
HongdaLin <lin.3235@osu.edu>
2021-06-16 15:32:42 -0400

    add basic functionalities for nevigate each page (Hongda)

--- Scraper.rb
+++ Scraper.rb
@@ -1,32 +1,85 @@
+# created by: Hongda Lin (Date: 6/16/2021)
+
 require 'mechanize'
-require 'nokogiri' #not used
-require 'open-uri' #not used
+require 'nokogiri'
 
-
-class Scraper
-  URL = "https://cse.osu.edu/course-coordinators"
+class Page
+  attr_reader :agent, :current_page
+  @@URL = "https://www.thelantern.com/campus/"
   def initialize
     @agent = Mechanize.new
-    @page = @agent.get URL
+    @currentPage = @agent.get @@URL
   end
 
+  # Created (Hongda Lin, 6/16)
+  # Requires: self.has_nextPage == true
+  # Set @currentPage to the next page of current page
+  def goto_nextPage
+    next_page_link = @agent.page.links.find{|link| link.text == "Next »"}
+    next_page_link.resolved_uri
+    @currentPage = next_page_link.click
+  end
+
+  # Created (Hongda Lin, 6/16)
+  # Requires: self.has_previousPage == true
+  # Set @currentPage to the previous page of current page
+  def goto_previousPage
+    previous_page_link = @agent.page.links.find{|link| link.text == "« Prev"}
+    previous_page_link.resolved_uri
+    @currentPage = previous_page_link.click
+  end
+
+  # Created (Hongda Lin, 6/16)
+  # Return true if there is a next page, false otherwise
+  def has_nextPage
+    @agent.page.links.find{|link| link.text == "Next »"} == nil ? false : true
+  end
+
+  # Created (Hongda Lin, 6/16)
+  # Return true if there is a next page, false otherwise
+  def has_previousPage
+    @agent.page.links.find{|link| link.text == "« Prev"} == nil ? false : true
+  end
+
+  # Created (Hongda Lin, 6/16)
+  #
+  # titles has class:  Nokogiri::XML::NodeSet
+  # each title inside titles has class: Nokogiri::XML::Element
+  #
+  # Return an array of strings (title)
+  #
+  # Problem: for each page the first three title are always the same
+  def currentPage_newsTitles
+    titles = @currentPage.xpath('//h2[@class="post-title"]/a')
+    arr_titles = Array.new
+    titles.each{|title| arr_titles<<title.text}
+    arr_titles
+  end
+
+  # Created (Hongda Lin, 6/16)
+  #
+  # links has class:  Nokogiri::XML::NodeSet
+  # each link inside links has class: Nokogiri::XML::Attr
+  #
+  # Return an array of strings (links)
+  #
+  # Problem: for each page the first three link are always the same
+  def currentPage_newsLinks
+    links = @currentPage.xpath('//h2[@class="post-title"]/a/@href')
+    arr_links = Array.new
+    links.each{|link| arr_links << link.text}
+    arr_links
+  end
+
+
 end
-agent = Mechanize.new
-page = agent.get "https://cse.osu.edu/course-coordinators"
 
-#document = Nokogiri::HTML.parse(open("https://cse.osu.edu/course-coordinators"))
+#Test
+page = Page.new
+page.goto_nextPage
+#puts page.has_previousPage
+#puts page.has_nextPage
+page.currentPage_newsLinks
 
-information = Hash.new
-titles = page.xpath("//th") # or use titles = page.css("th")
-titles.each do |title|
-  information[title.text.intern] = []
-end
 
-num_title = information.length
 
-bodies = page.xpath("//td")
-bodies.each_with_index do |body, index|
-    information[information.keys[index % num_title]] << body.text
-end
-
-print information
