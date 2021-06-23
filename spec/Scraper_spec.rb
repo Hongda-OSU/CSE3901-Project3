b1aff3f1a697f83b55cc86f7e0c15673218a71ea
Madison Graziani <graziani.16@osu.edu>
2021-06-22 23:23:26 -0400

    Added some Scraper tests

---
+++ spec/Scraper_spec.rb
@@ -0,0 +1,68 @@
+# Created by Madison Graziani on 6/22/2021
+# Spec to test the Scraper class
+require '../Scraper'
+require 'rspec/autorun'
+
+describe Scraper do
+
+  # Created by Madison Graziani on 6/22/2021
+  # @test
+  #   scrape_body
+  it "should return true" do
+    scraper = Scraper.new
+    scraper.scrape_reg_news
+    symbol = "Ohio State research review suggests strong connection between narcissism and aggression".to_sym
+    scraper.connect_page(scraper.reg_news[symbol])
+    content = scraper.scrape_body
+    expect(content.length > 0).to eq true
+  end
+
+  # Created by Madison Graziani on 6/22/2021
+  # @test
+  #   scrape_date
+  it "should return true" do
+    scraper = Scraper.new
+    scraper.scrape_reg_news
+    symbol = "Ohio State research review suggests strong connection between narcissism and aggression".to_sym
+    scraper.connect_page(scraper.reg_news[symbol])
+    date = scraper.scrape_date
+    expect(date == "June 16, 2021").to eq true
+  end
+
+  # Created by Madison Graziani on 6/22/2021
+  # @test
+  #   scrape_author
+  it "should return true" do
+    scraper = Scraper.new
+    scraper.scrape_reg_news
+    symbol = "Ohio State research review suggests strong connection between narcissism and aggression".to_sym
+    scraper.connect_page(scraper.reg_news[symbol])
+    name = scraper.scrape_author
+    expect(name == "Jessica Langer").to eq true
+  end
+
+
+  # Created by Madison Graziani on 6/22/2021
+  # @test
+  #   get_link and connect_page
+  it "should return true" do
+    scraper = Scraper.new
+    scraper.scrape_reg_news
+    title = "Ohio State research review suggests strong connection between narcissism and aggression"
+    link = scraper.get_link 3, title
+    scraper.connect_page(link)
+    expect(scraper.news_page != nil).to eq true
+  end
+
+  # Created by Madison Graziani on 6/22/2021
+  # @test
+  #   get_title
+  it "should return true" do
+    scraper = Scraper.new
+    scraper.scrape_reg_news
+    title = scraper.get_title 3, 1
+    link = scraper.get_link 3, title
+    scraper.connect_page(link)
+    expect(scraper.news_page != nil).to eq true
+  end
+end
