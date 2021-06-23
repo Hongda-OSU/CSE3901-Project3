a5dd4caf2328d5527e6f15e41b9799440549f2e4
HongdaLin <lin.3235@osu.edu>
2021-06-22 23:43:11 -0400

    add more test cases for methods in Scraper class

--- spec/Scraper_spec.rb
+++ spec/Scraper_spec.rb
@@ -65,4 +65,40 @@ describe Scraper do
     scraper.connect_page(link)
     expect(scraper.news_page != nil).to eq true
   end
+
+  # Created by Hongda Lin on 6/22/2021
+  # @test
+  #   scrape_mask_news
+  it "should Set @mask_news to a hash" do
+    scraper = Scraper.new
+    scraper.scrape_mask_news
+    expect(scraper.mask_news.instance_of? Hash).to eq true
+  end
+
+  # Created by Hongda Lin on 6/22/2021
+  # @test
+  #   scrape_trend_news
+  it "should Set @trend_news to a hash" do
+    scraper = Scraper.new
+    scraper.scrape_trend_news
+    expect(scraper.trend_news.instance_of? Hash).to eq true
+  end
+
+  # Created by Hongda Lin on 6/22/2021
+  # @test
+  #   scrape_reg_news
+  it "should Set @reg_news to a hash" do
+    scraper = Scraper.new
+    scraper.scrape_reg_news
+    expect(scraper.reg_news.instance_of? Hash).to eq true
+  end
+
+  # Created by Hongda Lin on 6/22/2021
+  # @test
+  #   create_regexp
+  it "should create a Regular expression with the terms provide" do
+    scraper = Scraper.new
+    expect(scraper.create_regexp "He").to eq /He/
+  end
+
 end
